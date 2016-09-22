8e004d292ee5d4205878858339d5e53cded218f2
KoJIT2009 <kojit2009@gmail.com>
2016-09-22 16:09:56 +0500

    feature/start
    Класс модуля.

---
+++ ShareLinks.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dolgikh
+ * Date: 22.09.16
+ * Time: 15:59
+ */
+
+namespace veksharelinks;
+
+use yii\base\Module;
+
+class ShareLinks extends Module {
+
+}
